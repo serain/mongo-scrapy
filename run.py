e65622ca56a4838e6ba6f8a3ed7922a7bc7d15d3
Alexandre Kaskasoli <alexandre.kaskasoli@mwrinfosecurity.com>
2018-08-24 12:36:08 +0100

    default to urls in db.apps if no input list provided

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import argparse
+from pymongo import MongoClient
 from scrapy.crawler import CrawlerRunner
 from scrapy.utils.log import configure_logging
 from twisted.internet import reactor
@@ -9,7 +10,7 @@ from spider import MongoSpider
 
 def get_args():
     parser = argparse.ArgumentParser(description='Process some integers.')
-    parser.add_argument('-iL', '--input-list', required=True,
+    parser.add_argument('-iL', '--input-list', required=False,
                         help='list of URLs to crawl')
     parser.add_argument('--mongo-host', default='127.0.0.1',
                         help='mongo host')
@@ -21,16 +22,24 @@ def get_args():
 
 def main():
     args = get_args()
+    urls = []
 
-    with open(args.input_list) as fh:
-        urls = fh.read().splitlines()
+    if args.input_list:
+        with open(args.input_list) as fh:
+            urls = fh.read().splitlines()
+    else:
+        client = MongoClient(host=args.mongo_host)
+        db = client[args.mongo_db]
+        urls = db.apps.distinct('url')
 
     configure_logging({'LOG_FORMAT': '%(levelname)s: %(message)s'})
     runner = CrawlerRunner({
                 'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)',
                 'MONGO_HOST': args.mongo_host,
                 'MONGO_DB': args.mongo_db,
-                'DEPTH_LIMIT': 4
+                'DEPTH_LIMIT': 1,
+                'DNS_TIMEOUT': 5,
+                'DOWNLOAD_TIMEOUT': 5, 
              })
 
 
