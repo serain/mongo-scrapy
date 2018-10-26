59245bd1c899c94e6a679529c723098a8b4ffd35
Alexandre Kaskasoli <alexandre.kaskasoli@mwrinfosecurity.com>
2018-10-26 16:44:45 +0100

    fix logging of exceptions

--- hashes.py
+++ hashes.py
@@ -3,10 +3,12 @@ import random
 import requests
 import hashlib
 import urllib3
+import logging
 from urllib.parse import urljoin
 from concurrent.futures import as_completed, ThreadPoolExecutor
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+logger = logging.getLogger(__name__)
 
 MAX_WORKERS = 5
 REQUEST_TIMEOUT = 5
@@ -30,13 +32,12 @@ def get_not_found_hashes(base_url):
     hashes = set()
 
     with ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
-        # Start the load operations and mark each future with its URL
         future_to_path = {executor.submit(fetch_hash, base_url, path): path for path in rand_paths}
         for future in as_completed(future_to_path):
             path = future_to_path[future]
             try:
                 hashes.add(future.result())
             except Exception as exc:
-                print(f'{path} generated an exception: {exc}')
+                logger.debug(f'{path} generated an exception: {exc}')
 
     return hashes
